8d148bc5d5699579ac373b2bbb637749060d5a87
MWis02 <marcin.wisniowski@opoczta.pl>
2024-03-07 10:11:53 +0100

    SQL v.1

---
+++ ships/MySQL.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/exception.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
+
+class MySQL
+{
+};
+
