d82fbb8b1d7012b6dec43e9f897106abd333484b
MWis02 <marcin.wisniowski@opoczta.pl>
2024-03-20 17:20:47 +0100

    SQL może działa

--- ships/MySQL.cpp
+++ ships/MySQL.cpp
@@ -1,1 +1,27 @@
 #include "MySQL.h"
+
+MySQL::MySQL() {
+    try {
+        // Po³¹czenie z baz¹ danych
+        driver = get_driver_instance();
+        con = driver->connect(IP, user, password);
+        con->setSchema(database);
+    }
+    catch (SQLException& e) {
+        // Obs³uga b³êdów
+        cout << "SQLException: " << e.what() << endl;
+    }
+    catch (std::runtime_error& e) {
+        // Obs³uga b³êdów
+        cout << "RuntimeError: " << e.what() << endl;
+    }
+}
+
+MySQL::~MySQL() {
+
+}
+
+void MySQL::connect_to_database(){
+    
+	return;
+}
--- ships/MySQL.h
+++ ships/MySQL.h
@@ -4,7 +4,26 @@
 #include <jdbc/cppconn/resultset.h>
 #include <jdbc/cppconn/statement.h>
 
+using namespace std;
+using namespace sql;
+
 class MySQL
 {
+private:
+	Driver *driver;
+	Connection* con;
+
+	Statement* stmt;
+	ResultSet* res;
+
+	string IP = "tcp://127.0.0.1:3306";
+	string user = "admin";
+	string password = "";
+	string database = "project_data_base";
+
+public:
+	MySQL();
+	~MySQL();
+	void connect_to_database();
 };
 
